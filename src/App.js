8092f177a51f9cf54c682a8361bc920f6f04f34d
hatae94 <gkxo5959@naver.com>
2022-03-07 22:35:07 +0900

    FEAT: 추가 socket 설정 및 게임 로직 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import GameBox from "./components/Game";
 
 function App() {
   const socore = useSelector((state) => state.gameResult.player1Score);
+  const [test, setTest] = useState(false);
+
   console.log(socore);
-  return <GameBox />;
+
+  function handelTest() {
+    setTest(true);
+  }
+
+  return (
+    <>
+      <button type="button" onClick={handelTest}>test</button>
+      {test && <GameBox />}
+    </>
+  );
 }
 
 export default App;
