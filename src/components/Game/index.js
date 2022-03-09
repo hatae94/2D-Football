ccfa7f1fa3d77ae524964ad8608c979e5d3104ab
hatae94 <gkxo5959@naver.com>
2022-03-10 02:43:55 +0900

    FEAT: 추가 골 스코어 화면 추가

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,16 +1,17 @@
 import Phaser from "phaser";
+
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import config from "../../phaser/config/config";
 
 import Timer from "../share/Timer";
+import ScoreBox from "../share/ScoreBox";
 
 export default function Game() {
   const time = useSelector((state) => state.room.roomInfo.time);
   let game = {};
 
-  // time 가져오기
   useEffect(() => {
     game = new Phaser.Game(config);
 
@@ -21,8 +22,11 @@ export default function Game() {
 
   return (
     <>
+      <>
+        <Timer seconds={time} />
+        <ScoreBox />
+      </>
       <div id="container" />
-      <Timer seconds={time} />
     </>
   );
 }
--- src/redux/middleware/socketMiddleware.js
+++ src/redux/middleware/socketMiddleware.js
@@ -1,20 +1,20 @@
 import {
-  setError, setIsAllReady, gameOver, setPlayTime,
+  setIsAllReady, gameOver, setOtherUserName,
 } from "../slice/roomSlice";
 
 import socket from "../../utils/socket";
 
 const socketMiddleware = () => (store) => {
-  socket.on("errorMessage", ({ message }) => {
-    store.dispatch(setError({ error: message }));
-  });
-
   socket.on("isAllReadyCheck", ({ isAllReady }) => {
     store.dispatch(setIsAllReady({ isAllReady }));
   });
 
   socket.on("gameOver", ({ isGameOver }) => {
     store.dispatch(gameOver({ isGameOver }));
+  });
+
+  socket.on("sendPlayersNameList", (playersNameList) => {
+    store.dispatch(setOtherUserName(playersNameList));
   });
 
   return (next) => (action) => {
