47731acd8b96a78f0bbb7206f9e53ac064a0ba26
hatae94 <gkxo5959@naver.com>
2022-03-12 05:45:29 +0900

    REFACTOR: 추가 withCredential 설정

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io.connect(process.env.REACT_APP_SERVER_PORT);
+const socket = io.connect(process.env.REACT_APP_SERVER_PORT, {
+  withCredentials: true,
+});
 
 export default socket;
